48eef202680fde123ed3bf8a8f9901824a5de632
umi <asiessoyeltier1@gmail.com>
2025-05-04 21:55:24 -0500

    [task] finish helper functions

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -6,7 +6,7 @@ function HelperFunctions() {
 
 	//event handler for the clear button event. Clears the screen
 	select("#clearButton").mouseClicked(function() {
-		//???
+		background(255);
 
 		//call loadPixels to update the drawing state
 		//this is needed for the mirror tool
@@ -16,6 +16,6 @@ function HelperFunctions() {
 	//event handler for the save image button. saves the canvsa to the
 	//local file system.
 	select("#saveImageButton").mouseClicked(function() {
-		//???
+		saveCanvas("beautifulart");//???
 	});
 }
