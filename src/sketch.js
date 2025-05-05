df5198ebbe64f7844f3018072ff6f84dbc64f7cd
umi <asiessoyeltier1@gmail.com>
2025-05-04 21:47:40 -0500

    [task] rewrite spray can tool
    Every script were renamed to src and it's property updated on the
    index.html; additionally, the latest version of p5.min.js is being fetch
    from the npm official p5 repository.

--- src/sketch.js
+++ src/sketch.js
@@ -4,25 +4,6 @@ var toolbox = null;
 var colourP = null;
 var helpers = null;
 
-//spray can object literal
-sprayCan = {
-    name: "sprayCanTool",
-    icon: "assets/sprayCan.jpg",
-    points: 13,
-    spread: 10,
-    draw: function(){
-        //if the mouse is pressed paint on the canvas
-        //spread describes how far to spread the paint from the mouse pointer
-        //points holds how many pixels of paint for each mouse press.
-        if(mouseIsPressed){
-            for(var i = 0; i < this.points; i++){
-                point(random(mouseX-this.spread, mouseX + this.spread), 
-                    random(mouseY-this.spread, mouseY+this.spread));
-            }
-        }
-    }
-};
-
 function setup() {
 
 	//create a canvas to fill the content div from index.html
@@ -40,7 +21,7 @@ function setup() {
 	//add the tools to the toolbox.
 	toolbox.addTool(new FreehandTool());
 	toolbox.addTool(new LineToTool());
-	toolbox.addTool(sprayCan);
+	toolbox.addTool(new spraycanTool());
 	toolbox.addTool(new mirrorDrawTool());
 	background(255);
 
